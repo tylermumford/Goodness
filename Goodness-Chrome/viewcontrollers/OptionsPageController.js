ba93898d7c17463009b42526b001757e99902955
Tyler Wayne <tyler.wayne.dev@icloud.com>
2013-04-17 22:43:34 -0600

    Added (empty) OptionsPageController.js.

---
+++ Goodness-Chrome/viewcontrollers/OptionsPageController.js
@@ -0,0 +1,9 @@
+// This file is under copyright. See the COPYING.txt file for more information.
+
+(function()) {
+  var Store = new StoreController();
+
+  var toggleEntryMode = function() {
+    
+  }
+}())
